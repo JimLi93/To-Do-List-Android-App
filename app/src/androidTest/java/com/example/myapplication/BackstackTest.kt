044ae6b3bb596a708d92681fd97476ab9e80d80b
Yang, Jhih-Ming <s109062319@m109.nthu.edu.tw>
2022-06-11 09:58:39 +0800

    Added more backstack tests and candidate tests

--- app/src/androidTest/java/com/example/myapplication/BackstackTest.kt
+++ app/src/androidTest/java/com/example/myapplication/BackstackTest.kt
@@ -1,7 +1,8 @@
 package com.example.myapplication
 
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.Lifecycle.State
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
@@ -14,7 +15,7 @@ import org.junit.Test
 
 class BackstackTest {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
     lateinit var navController: NavHostController
     private lateinit var uiDevice: UiDevice
 
@@ -152,5 +153,89 @@ class BackstackTest {
             navController.currentBackStackEntry?.destination?.route?.startsWith("taskdetail")
                 ?: false
         )
+    }
+
+    @Test
+    fun taskHistoryBackstackTest() {
+        // Click the history button
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("TASK"))
+        ).performClick()
+        // Click the back arrow
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("HISTORY"))
+        ).performClick()
+        // Press the back button
+        uiDevice.pressBack()
+        assertTrue(navController.currentBackStackEntry?.destination?.route != "historypage")
+    }
+
+    @Test
+    fun taskDetailsHistoryBackstackTest() {
+        // Click the history button
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("TASK"))
+        ).performClick()
+        composeTestRule.onNodeWithText("Task1").performClick()
+        // Click the back arrow
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("DETAILS"))
+        ).performClick()
+        // Press the back button
+        uiDevice.pressBack()
+        // Check that the current screen is not a task details in history
+        assertFalse(
+            navController.currentBackStackEntry?.destination?.route?.startsWith("taskdetailhistory")
+                ?: false
+        )
+    }
+
+    // fun addTaskBackstackTest
+    // fun editTaskBackstackTest
+    // fun deleteTaskBackstackTest
+    // fun deleteTaskHistoryBackstackTest
+    // fun completeTaskBackstackTest
+    // fun swipeTaskBackstackTest
+
+    @Test
+    fun readStoryBackstackTest() {
+        composeTestRule.onNode(hasClickAction(), true).performClick()
+        composeTestRule.onNodeWithText("Story").performClick()
+        composeTestRule.onNodeWithText("1-1").performClick()
+        // Click the back arrow
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("Chapter1-1"))
+        ).performClick()
+        // Press the back button
+        uiDevice.pressBack()
+        // Check that the current screen is not a story read
+        assertFalse(
+            navController.currentBackStackEntry?.destination?.route?.startsWith("readstory")
+                ?: false
+        )
+    }
+
+    @Test
+    fun storyMapBackstackTest() {
+        val composeLifecycle = composeTestRule.activity.lifecycle
+        composeTestRule.onNode(hasClickAction(), true).performClick()
+        composeTestRule.onNodeWithText("Story").performClick()
+        // Click the chapter button
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("STORY MAP"))
+        ).performClick()
+        // Select chapter 1
+        composeTestRule.onNodeWithText("1").performClick()
+        // Click the back arrow
+        composeTestRule.onNode(
+            hasClickAction() and isEnabled() and hasAnySibling(hasTextExactly("Chapter1-1"))
+        ).performClick()
+        // Press the back button
+        uiDevice.pressBack()
+        Thread.sleep(1500)
+        // TODO: the name of the destinations to check
+        val isAtStoryPage =
+            navController.currentBackStackEntry?.destination?.route==BottomBarScreen.StoryMap.route
+        assertFalse(isAtStoryPage && composeLifecycle.currentState != State.DESTROYED)
     }
 }
